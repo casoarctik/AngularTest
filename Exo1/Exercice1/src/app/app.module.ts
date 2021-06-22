2183bde2d1699f23fc6467299c8ed64ee8829f0d
casoarctik <colin.beaumont@outlook.fr>
2021-06-22 15:42:31 +0200

    exo1 done

--- Exo1/Exercice1/src/app/app.module.ts
+++ Exo1/Exercice1/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { ListComponentComponent } from './list-component/list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
